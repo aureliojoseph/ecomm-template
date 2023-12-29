84487fae7ad10f9203d3b7711c72b3562387cf8a
Aurelio Borges <aurelioaborges@gmail.com>
2023-12-29 11:13:56 -0300

    create: Nuxt pages

---
+++ pages/products/index.vue
@@ -0,0 +1,14 @@
+<template>
+	<div>
+		<h3>Products Page</h3>
+	</div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+	h3 {
+		margin-bottom: 20px;
+		font-size: 32px;
+	}
+</style>
